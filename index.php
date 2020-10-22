6683ece431c7d1b54bd58a1ad1508d38bf3a7a33
Alessandro <alessandro.boscato@gmail.com>
2020-10-22 16:08:44 +0200

    setEmail method

--- index.php
+++ index.php
@@ -9,6 +9,13 @@ class User
   public $email;
   public $username;
   public $password;
+  public $eta = 0;
+
+  public function setEmail() {
+    if ($email == null) {
+      $this->email = "$this->name"."."."$this->lastname"."@gmail.com";
+    }
+  }
 }
 
 $user1 = new User();
@@ -16,9 +23,13 @@ $user1->name = "Alessandro";
 $user1->lastname = "Boscato";
 
 
+
+
 $user2 = new User();
 $user2->name = "Carlo";
 $user2->lastname = "Petrolio";
+$user2->setEmail();
+var_dump($user1, $user2);
 
 
 // $user1
