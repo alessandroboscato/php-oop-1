e8583c154a364b087d940063acabe166aac16f30
Alessandro <alessandro.boscato@gmail.com>
2020-10-22 15:45:16 +0200

    inseriti manualmente 2 utenti

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ *
+ */
+class User
+{
+  public $name;
+  public $lastname;
+  public $email;
+  public $username;
+  public $password;
+}
+
+$user1 = new User();
+$user1->name = "Alessandro";
+$user1->lastname = "Boscato";
+
+
+$user2 = new User();
+$user2->name = "Carlo";
+$user2->lastname = "Petrolio";
+
+
+// $user1
+ ?>
